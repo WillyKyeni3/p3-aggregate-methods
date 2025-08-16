61f68007ee6152b614735285590bf0ab8c87981a
willymusyoka <willykyeni@gmail.com>
2025-08-16 14:10:26 +0300

    Adds aggregate methods

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+
 class Student:
-    def __init__(self, name):
+    def __init__(self, name, _grades):
         self.name = name
+        self._grades = _grades
         self._enrollments = []
 
     def enroll(self, course):
@@ -11,6 +13,17 @@ class Student:
             course.add_enrollment(enrollment)
         else:
             raise TypeError("course must be an instance of Course")
+        
+    def aggregate_average_grade(self):
+      # lets assume the grades are stored in a protected attribute called _grades. 
+      total_grades = sum(self._grades.values())
+      num_courses = len(self._grades)
+      average_grade = total_grades / num_courses
+
+      return average_grade
+
+    def course_count(self):
+        return len(self._enrollments)
 
     def get_enrollments(self):
         return self._enrollments.copy()
@@ -42,6 +55,14 @@ class Enrollment:
             type(self).all.append(self)
         else:
             raise TypeError("Invalid types for student and/or course")
+        
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
 
     def get_enrollment_date(self):
         return self._enrollment_date
